eb3caa958147da26709ed7826b27ffbcf49a7bd2
Ethan <ethanjmelton24@gmail.com>
2025-07-22 21:47:51 -0500

    #New menu and added the ability to have multiple accounts for this bank, and am able to search through the accounts

--- BankSystem.cpp
+++ BankSystem.cpp
@@ -4,6 +4,8 @@
 #include <iomanip>
 #include <sstream>  // for ostringstream
 #include <limits> // for numeric_limits
+#include <vector>
+
 
 // This is the branch file for the Bank System project
 
@@ -14,13 +16,17 @@ class bank
 {
     char name[100], address[100], acc_type;
     float amount, temp;
+    int account_number;
+    static int account_count; // Static variable to keep track of the number of accounts
     string balance; //Is currently being used to put in the starting amount
     public:
         void open_account();
         void deposit_money();
         void withdraw_money();
         void display_account();
+        int get_account_number() const { return account_number; }
 };
+
 
 
 //Need to get all number values as strings and convert them to digits when needed
@@ -67,6 +73,9 @@ void bank :: open_account()
         }
     } while (count != balance.size());
 ///////////////////////////////////////////////////////////////////////////////////////////////
+
+    account_number = account_count++;
+    cout << "Your account number is: " << account_number << endl;
 
     cout << "Your account has been created \n";
 
@@ -117,41 +126,65 @@ void bank :: withdraw_money()
 
     cout << "Now total amount left: " << balance;
 }
+
+// Function to find an account by account number
+// Returns a pointer to the account if found, otherwise returns nullptr
+bank* find_account(vector<bank>& accounts, int acct_num) {
+    for (auto& acc : accounts) {
+        if (acc.get_account_number() == acct_num) {
+            return &acc;
+        }
+    }
+    return nullptr;
+}
+
+int bank::account_count = 1000; // starting account number
+
 int main()
 {
-    cout << "Branch Test\n";
-    
+    //cout << "Branch Test\n";
     int selection;
     bank obj;
-
-    do
+    vector<bank> accounts;
+    
+    cout << "Welcome to the Bank System\n";
+    cout << "-----------------------------------\n";
+    do 
     {
-        cout << endl << "1) Open Account \n";
-        cout << "2) Deposit Money \n";
-        cout << "3) Withdraw Money \n";
-        cout << "4) Display Account Balance \n";
-        cout << "5) Exit \n";
-        cout << "Select from the options above: ";
+        cout << "\n1) Open Account\n2) Deposit\n3) Withdraw\n4) Display\n5) Exit\nSelection: ";
         cin >> selection;
 
-        switch(selection)
+        if (selection == 1) 
         {
-            case 1: cout << "\n1) Open Account \n";
-            obj.open_account();
-            break;
-            case 2: cout << "\n2) Deposit Money \n";
-            obj.deposit_money();
-            break;
-            case 3: cout << "\n3) Withdraw Money \n";
-            obj.withdraw_money();
-            break;
-            case 4: cout << "\n4) Display Account Balance \n";
-            obj.display_account();
-            break;
-            case 5: 
-                cout << "\nGoodbye";
-                break;
-            default: cout << "Invalid Selection";
+            bank new_acc;
+            new_acc.open_account();
+            accounts.push_back(new_acc);
+        } 
+        else if (selection >= 2 && selection <= 4) 
+        {
+            int acct_num;
+            cout << "Enter your account number: ";
+            cin >> acct_num;
+
+            bank* acc = find_account(accounts, acct_num);
+            if (!acc) {
+                cout << "Account not found.\n";
+                continue;
+            }
+
+            switch (selection) {
+                case 2: acc->deposit_money(); break;
+                case 3: acc->withdraw_money(); break;
+                case 4: acc->display_account(); break;
+            }
+        }
+        else if (selection == 5) 
+        {
+            cout << "Goodbye\n";
+        } 
+        else 
+        {
+            cout << "Invalid selection.\n";
         }
     } while (selection != 5);
     return 0;
